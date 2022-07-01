513fcad176af9f5b23d3e3f5de54438f7e6fe179
my_centos8 <458009848@qq.com>
2022-07-01 11:10:49 +0800

    服务器上第一次改动

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i^)$mo8_co(%!8q3=74m5&i_v8j7f@*wv@(wh_19&r=dpr9wm='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','139.196.111.99']
 
 
 # Application definition
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'comments'
+    'blog.apps.BlogConfig',
+    'comments.apps.CommentsConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- blogproject/comments/templatetags/comment_extras.py
+++ blogproject/comments/templatetags/comment_extras.py
@@ -4,7 +4,7 @@ from comments.forms import CommentForm
 
 register = template.Library()
 
-@register.inclusion_tag(r'comment\inclusions\_form.html',takes_context=True)
+@register.inclusion_tag('comment/inclusions/_form.html',takes_context=True)
 def show_comments_form(context, post, form=None):
     if form is None:
         form = CommentForm()
@@ -15,7 +15,7 @@ def show_comments_form(context, post, form=None):
         'post': post,
     }
 
-@register.inclusion_tag(r'comment\inclusions\_comment.html',takes_context=True)
+@register.inclusion_tag('comment/inclusions/_comment.html',takes_context=True)
 def show_comments(context, post):
     comment_list = post.comment_set.all().order_by('-created_time')
     comment_count = comment_list.count()
